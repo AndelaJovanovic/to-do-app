738f8d85014c6be3de1a2ac07efd3fff5249314b
Andela Jovanovic <kendjelka@Andelas-MacBook-Pro.local>
2021-01-31 17:17:14 +0100

    can finally check tasks as completed yay

--- app.js
+++ app.js
@@ -19,16 +19,17 @@ function addTodo (e){
     addTodo.innerText = todoInput.value
     addTodo.classList.add('todo-item')
     todoContainer.appendChild(addTodo)
+    todoInput.value = ""
 
     // COMPLETED BUTTON
     const completed = document.createElement('button')
-    completed.innerHTML = '<i class="fas fa-check"></i>'
+    completed.innerHTML = `<i class="fas fa-check"></i>`
     completed.classList.add("completed-button")
     todoContainer.appendChild(completed)
 
      // ELIMINATE BUTTON
      const eliminate = document.createElement('button')
-     eliminate.innerHTML = '<i class="far fa-trash-alt"></i>'
+     eliminate.innerHTML =`<i class="far fa-trash-alt"></i>`
      eliminate.classList.add("eliminate-button")
      todoContainer.appendChild(eliminate)
 
@@ -42,19 +43,20 @@ function addTodo (e){
 //DELETE FUNCTION FOR TODOS
 function deleteTodo(e){
     const item = e.target
+
     if(item.classList[0] === 'eliminate-button'){
        const todo = item.parentElement
        todo.classList.add("bye-task")
-       todo.addEventListener('transitioned', function(){
+       todo.addEventListener('transitioned', e =>{
            todo.remove()
        })
     }
-}
 
 // MARK TODOS AS COMPLETED
 if(item.classList[0] === 'completed-button'){
     const todo = item.parentElement
     todo.classList.toggle("done")
+}
 }
 
 function filterTodo(e){
@@ -67,6 +69,12 @@ function filterTodo(e){
                 case "completed":
                 if(todo.classList.contains('completed')){
                     todo.style.display='flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+            case 'undone':
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex'
                 } else {
                     todo.style.display = 'none'
                 }
