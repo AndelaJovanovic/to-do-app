fe8ae3cceb265ea0b503c9e56564634358da2b0c
Andela Jovanovic <kendjelka@Andelas-MacBook-Pro.local>
2021-01-31 15:24:25 +0100

    trying to manage the lists created in html, next up is to add functionality to buttons completed and delete

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const todoList = document.querySelector('.todo-list')
 
 todoButton.addEventListener('click', addTodo)
 
-function addTodo(e){
+const addTodo = (e) => {
     e.preventDefault()
     console.log('are you there?')
+    const todoContainer = document.createElement('div')
+    todoContainer.classList.add('todo')
+
+    const addTodo = document.createElement('li')
+    addTodo.innerText = 'hello, task'
+    addTodo.classList.add('todo-item')
+    todoContainer.appendChild(addTodo)
 }
